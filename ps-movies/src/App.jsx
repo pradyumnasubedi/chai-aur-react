d8695912bf93500bf2336c33ece50d3d35a3581d
pradyumnasubedi <testerpradyumna@gmail.com>
2024-12-31 13:10:05 +0545

    learn dynamic rendering of arrays

--- ps-movies/src/App.jsx
+++ ps-movies/src/App.jsx
@@ -1,26 +1,10 @@
 import "./App.css"
-import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
 function App() {
   const movieNumber = 1
   return (
     <>
-      {movieNumber === 1 ? (
-        <MovieCard
-          movie={{
-            title: "Carry-On",
-            release_date: "2024",
-            url: "www.google.com",
-          }}
-        />
-      ) : (
-        <MovieCard
-          movie={{
-            title: "Bohemian Rhapsody",
-            release_date: "2018",
-            url: "www.google.com",
-          }}
-        />
-      )}
+      <Home />
     </>
   )
 }
