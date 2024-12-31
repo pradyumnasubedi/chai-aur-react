8f29e3321248d84d173654593b411009bed0dd24
pradyumnasubedi <testerpradyumna@gmail.com>
2024-12-31 14:09:31 +0545

    learn react page routing

--- ps-movies/src/App.jsx
+++ ps-movies/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css"
 import Home from "./pages/Home"
+import Favorites from "./pages/Favorites"
+import { Routes, Route } from "react-router-dom"
+
 function App() {
-  const movieNumber = 1
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ ps-movies/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h3>No favorites here</h3>
+      <p>Add your favorites from the homepage and they will appear here!</p>
+    </div>
+  )
+}
+
+export default Favorites
